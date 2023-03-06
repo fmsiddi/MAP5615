c884d2d6aa21ae2aa65bb02f032d1cead47a981d
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2023-03-06 16:15:59 -0500

    update 1
    fixed vdc

--- MAP5615_HW2.py
+++ MAP5615_HW2.py
@@ -7,11 +7,19 @@ primes = list(sympy.primerange(2,200))
 
 def vdc(n,b):
     digs = list(np.base_repr(n,b))
+    len_digs = len(digs)
     sum = 0
-    for i in range(len(digs)):
-        sum += int(digs[i])/b**i
+    for i in range(len_digs):
+        sum += int(digs[i])/b**(len_digs-i)
     return sum
+
+vdc_test = np.array([vdc(n,2) for n in range(10)])
 
 def halton(n,s):
     return [vdc(n,i) for i in primes[:s]]
 
+
+# PRODUCING 1000 2-DIMENSIONAL HALTON VECTORS
+hal_2d_1000 = np.array([halton(n,2) for n in range(1,1001)])
+
+plt.scatter(hal_2d_1000[:,0],hal_2d_1000[:,1])
