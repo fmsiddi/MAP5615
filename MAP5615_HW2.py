7751af8824b724b70b86545dd6a3f10ade79fc38
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2023-03-06 22:26:59 -0500

    update 4
    I think first problem is finished correctly now

--- MAP5615_HW2.py
+++ MAP5615_HW2.py
@@ -13,24 +13,37 @@ def vdc(n,b):
         sum += int(digs[i])/b**(len_digs-i)
     return sum
 
-vdc_1000 = np.array([vdc(n,2) for n in range(1,1001)])
+vdc_4000 = np.array([vdc(n,2) for n in range(1,4001)])
 
 def halton(n,s):
     return [vdc(n,i) for i in primes[:s]]
 
-
-# PRODUCING 1000 2-DIMENSIONAL HALTON VECTORS
-hal_2d_1000 = np.array([halton(n,2) for n in range(1,1001)])
-
-plt.scatter(hal_2d_1000[:,0],hal_2d_1000[:,1])
+# plt.scatter(hal_2d_1000[:,0],hal_2d_1000[:,1])
 
 #The following function assumes F(x) is the cdf for the uniform distribution with a = 0 and b = 1
 def A_2_max(X,t):
     N = int(len(X)/t)
-    Z = np.sort(X) # Normally we would compute Z = F(X_i), which in this case is simply X_i
-    V = np.array([max(Z[(i*t):((i+1)*t)]) for i in range(N)])
-    var = [(2*(i+1)-1)*(np.log(V[i])+np.log(1-V[N-i-1])) for i in range(N)]
+    # Z = np.sort(X) # Normally we would compute Z = F(X_i), which in this case is simply X_i
+    V = np.array([max(X[(i*t):((i+1)*t)]) for i in range(N)])
+    Z = np.sort(V)
+    var = [(2*(i+1)-1)*(np.log(Z[i]**t)+np.log(1-Z[N-i-1]**t)) for i in range(N)]
     return - N - (1/N)*np.sum(var)
 
-test = A_2_max(vdc_1000,4)
+test = A_2_max(vdc_4000,4) # .008233
 
+
+# This is similar to the previous function but takes in a halton sequence
+def A_2_max_halton(X):
+    t = X.shape[1]
+    N = X.shape[0]
+    V = np.array([max(X[i]) for i in range(N)])
+    Z = np.sort(V)
+    var = [(2*(i+1)-1)*(np.log(Z[i]**t)+np.log(1-Z[N-i-1]**t)) for i in range(N)]
+    return - N - (1/N)*np.sum(var)
+
+# PRODUCING 1000 2-DIMENSIONAL HALTON VECTORS
+hal_4d_1000 = np.array([halton(n,4) for n in range(1,1001)])
+
+test = A_2_max_halton(hal_4d_1000) # .008233
+
+#%%
