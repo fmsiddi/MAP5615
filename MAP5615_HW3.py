1365cc4017881de90bf6158dc7ed1a66a2e401bf
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2023-03-28 02:17:18 -0400

    update 1
    added box muller, monte carlo pricing

--- MAP5615_HW3.py
+++ MAP5615_HW3.py
@@ -1,6 +1,7 @@
 # Libraries used:
 import numpy as np
 from scipy.stats import norm
+import random as rnd
 import sympy
 import re
 from math import factorial
@@ -60,6 +61,104 @@ for i in range(s):
     for j in range(k):
         rperms[i][j] = np.insert(np.random.permutation(np.arange(1,b)),0,0)
 
+#%%
+
+# def box_muller():
+#     R = np.sqrt(-2 * np.log(np.random.uniform()))
+#     θ = 2 * np.pi * np.random.uniform()
+#     X = R * np.cos(θ)
+#     Y = R * np.sin(θ)
+#     return X, Y
+
+def box_muller():
+    u1 = rnd.random()
+    u2 = rnd.random()
+    R = np.sqrt(-2 * np.log(u1))
+    θ = 2 * np.pi * u2
+    X = R * np.cos(θ)
+    Y = R * np.sin(θ)
+    return X, Y
+
+#%%
+
+def GBM(S,T,n,r,σ):
+    Δt = T/n
+    S_t = np.zeros((2,n))
+    S_t[0] = S
+    S_t[1] = S
+    for i in range(1,n):
+        Z1, Z2 = box_muller()
+        S_t[0][i] = S_t[0][i-1] * np.exp((r - .5*σ**2)*Δt + σ*np.sqrt(Δt)*Z1)
+        S_t[1][i] = S_t[1][i-1] * np.exp((r - .5*σ**2)*Δt + σ*np.sqrt(Δt)*Z2)
+    return S_t
+
+n = 10
+num = 10000
+S = 50
+T = 1
+r = .1
+σ = .3
+K = 50
+paths = np.zeros((num,n))
+for i in np.arange(0,num,2):
+    paths[i:i+2] = GBM(S,T,n,r,σ)
+    
+# plt.plot(paths.T)
+# plt.plot(GBM(S,T,n,r,σ))
+# GBM(S,T,n,r,σ)
+
+avg_S = paths[:,-1].mean()
+approx_px = max(avg_S - K, 0)
+        
+#%%
+
+def GBM(S,T,n,r,σ):
+    Δt = T/n
+    S_t = np.zeros(n)
+    S_t[0] = S
+    for i in range(1,n):
+        Z1, Z2 = box_muller()
+        S_t[i] = S_t[i-1] * np.exp((r - .5*σ**2)*Δt + σ*np.sqrt(Δt)*Z1)
+    return S_t
+
+n = 10
+num = 10000
+S = 50
+T = 1
+r = .1
+σ = .3
+K = 50
+paths = np.zeros((num,n))
+for i in range(num):
+    paths[i] = GBM(S,T,n,r,σ)
+    
+# plt.plot(paths.T)
+# plt.plot(GBM(S,T,n,r,σ))
+# GBM(S,T,n,r,σ)
+
+payoffs = paths[:,-1]-K
+payoffs = np.maximum(payoffs,0)
+approx_px = payoffs.mean()
+print(approx_px)
+
+#%%
+
+# n = 50000
+# test = np.zeros(2*n)
+# for i in np.arange(0,n,2):
+#     Z1, Z2 = box_muller()
+#     test[i] = Z1
+#     test[i+1] = Z2
+    
+n = 100000
+test = np.zeros(n)
+for i in range(n):
+    Z1, Z2 = box_muller()
+    test[i] = Z2
+
+plt.hist(test,bins=50)
+
+
 
 #%%
 
@@ -73,7 +172,6 @@ def vdc(n,b):
 
 def halton(n,s):
     return [vdc(n,i) for i in primes[:s]]
-
 
 
 
