15511d7d8d088da3580a968120506603e6b398ae
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2023-04-27 21:16:54 -0400

    rf
    starting to (try to) implement random forest

--- LS Monte Carlo.py
+++ LS Monte Carlo.py
@@ -1,57 +1,10 @@
 import numpy as np
 from numpy import random as rnd
 from scipy.stats import norm
+from sklearn.ensemble import RandomForestRegressor
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
-#%%
-
-paths = np.array([[1,1.09,1.08,1.34],
-                  [1,1.16,1.26,1.54],
-                  [1,1.22,1.07,1.03],
-                  [1,.93,.97,.92],
-                  [1,1.11,1.56,1.52],
-                  [1,.76,.77,.9],
-                  [1,.92,.84,1.01],
-                  [1,.88,1.22,1.34]])
-# paths = np.array([[1,1.09,1.08,1.34],
-#                   [1,1.16,1.26,1.54],
-#                   [1,1.02,1.07,1.03],
-#                   [1,.93,.97,.92],
-#                   [1,1.11,1.56,1.52],
-#                   [1,.76,.77,.9],
-#                   [1,.92,.84,1.01],
-#                   [1,.88,1.22,1.34]])
-T = 3
-N = 8
-K=1.1
-r=.06
-Δt = 1
-M = int(T/Δt + 1)
-t = np.linspace(0,T,M)
-cf = np.zeros((N,M))
-cf[:,-1] = np.where(K-paths[:,-1]<0,0,K-paths[:,-1])
-# for i in range(M-1,0,-1):
-for i in range(M-1,0,-1):
-    cf_index = np.where(K-paths[:,i-1]>0)[0]
-    X = paths[np.where(K-paths[:,i-1]>0),i-1][0]
-    Y = np.zeros(X.shape[0])
-    for m in range(i,M):
-        Y += cf[np.where(K-paths[:,i-1]>0),m][0]*np.exp(-r*Δt*(m-i+1))
-    
-    model = np.poly1d(np.polyfit(X, Y, 2))
-    exercise = K - X
-    continuation = model(X)
-    index = np.where(exercise-continuation>0)[0]
-    cf[cf_index[index],i-1] = exercise[index]
-    cf[:,i] = np.where(cf[:,i]-cf[:,i-1]==cf[:,i],cf[:,i],0)
-    for n in range(N):
-        cf[n,:] = np.where(cf[n,:].max()-cf[n,:]>0,0,cf[n,:])
-px = 0
-for i in range(1,M):
-    px += sum(cf[:,i]*np.exp(-r*t[i]))
-px = px/N
-print(px)
 
 #%%
 # black-scholes price
@@ -60,107 +13,65 @@ def BS(S,K,T,t,r,σ):
     d2 = d1 - (σ*np.sqrt(T-t))
     return S*norm.cdf(d1) - K*np.exp(-r*(T-t))*norm.cdf(d2) 
 
-def LSM_put(S,K,T,r,σ,M,N):
+#%%
+S = 100
+K = 110
+T = 1
+r = .1
+σ = .25
+m = 10
+M = int(m*T)
+N = 100000
+poly_degree = 6
+
+def put(S,K,T,r,σ,M,N,method):
     Δt = T/M
-    Z = rnd.normal(size=(N,M))
+    discount_factor = np.exp(-r * Δt)
+    Z = rnd.normal(size=(int(N/2),M))
+    Z = np.concatenate((Z,-Z))
     S_t = np.zeros((N,M+1)) # create matrix for all paths
     S_t[:,0] = S # intitialize S_0 for all paths
     a1 = (r - .5*σ**2)*Δt # constant calculated outside of loop to reduce flops required
     a2 = σ*np.sqrt(Δt) # constant calculated outside of loop to reduce flops required
     for k in range(M):
         S_t[:,k+1] = S_t[:,k] * np.exp(a1 + a2*Z[:,k])
-    
-    cf = np.zeros(S_t.shape)
-    cf[:,-1] = np.where(K-S_t[:,-1]<0,0,K-S_t[:,-1])
-    for i in range(M,0,-1):
-        cf_index = np.where(K-S_t[:,i-1]>0)[0]
-        X = S_t[np.where(K-S_t[:,i-1]>0),i-1][0]
-        Y = np.zeros(X.shape[0])
-        for m in range(i,M+1):
-            Y += cf[np.where(K-S_t[:,i-1]>0),m][0]*np.exp(-r*Δt*(m-i+1))
-        # Y = cf[np.where(K-S_t[:,i-1]>0),i][0]*np.exp(-r*Δt)
         
-        model = np.poly1d(np.polyfit(X, Y, 2))
-        exercise = K - X
-        continuation = model(X)
-        index = np.where(exercise-continuation>0)[0]
-        cf[cf_index[index],i-1] = exercise[index]
-        cf[:,i] = np.where(cf[:,i]-cf[:,i-1]==cf[:,i],cf[:,i],0)
-        for n in range(N):
-            cf[n,i:] = np.where(cf[n,i:].max()-cf[n,i:]>0,0,cf[n,i:])
+    payoff = np.maximum(K - S_t, np.zeros_like(S_t))
+    value = np.zeros_like(payoff)
+    value[:, -1] = payoff[:, -1]
     
-    t = np.linspace(0,T,M+1)
-    px = 0
-    
-    for i in range(1,M+1):
-        px += sum(cf[:,i]*np.exp(-r*t[i]))
-    px = px/N
-    return px
+    for t in tqdm(range(M-1 , 0 , -1)):
+        if method == 'LS':
+            regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
+            continuation_value = np.polyval(regression, S_t[:, t])
+        elif method == 'RF':
+            regression = RandomForestRegressor(n_estimators=10, max_depth=5, min_samples_leaf=100)
+            continuation_value = regression.fit(S_t[:, t], value[:, t + 1]*discount_factor)
+        value[:, t] = np.where(
+            payoff[:, t] > continuation_value,
+            payoff[:, t],
+            value[:, t + 1] * discount_factor
+        )
+    option_premium = np.mean(value[:, 1] * discount_factor)
+    return option_premium
 
 #%%
 
-S = 36
-K = 40
+S = 100
+K = 110
 T = 1
-r = .06
-σ = .2
-m = 50
+r = .1
+σ = .25
+m = 10
 M = int(m*T)
 N = 100000
+poly_degree = 6
+method = 'RF'
 
-Δt = T/M
-Z = rnd.normal(size=(N,M))
-S_t = np.zeros((N,M+1)) # create matrix for all paths
-S_t[:,0] = S # intitialize S_0 for all paths
-a1 = (r - .5*σ**2)*Δt # constant calculated outside of loop to reduce flops required
-a2 = σ*np.sqrt(Δt) # constant calculated outside of loop to reduce flops required
-for k in range(M):
-    S_t[:,k+1] = S_t[:,k] * np.exp(a1 + a2*Z[:,k])
-#%%
-
-cf = np.zeros(S_t.shape)
-cf[:,-1] = np.where(K-S_t[:,-1]<0,0,K-S_t[:,-1])
-for i in tqdm(range(M,0,-1)):
-    cf_index = np.where(K-S_t[:,i-1]>0)[0]
-    X = S_t[np.where(K-S_t[:,i-1]>0),i-1][0]
-    Y = np.zeros(X.shape[0])
-    for m in range(i,M+1):
-        Y += cf[np.where(K-S_t[:,i-1]>0),m][0]*np.exp(-r*Δt*(m-i+1))
-    # Y = cf[np.where(K-S_t[:,i-1]>0),i][0]*np.exp(-r*Δt)
-    
-    model = np.poly1d(np.polyfit(X, Y, 2))
-    exercise = K - X
-    continuation = model(X)
-    index = np.where(exercise-continuation>0)[0]
-    cf[cf_index[index],i-1] = exercise[index]
-    cf[:,i] = np.where(cf[:,i]-cf[:,i-1]==cf[:,i],cf[:,i],0)
-    for n in range(N):
-        cf[n,i:] = np.where(cf[n,i:].max()-cf[n,i:]>0,0,cf[n,i:])
-
-t = np.linspace(0,T,M+1)
-px = 0
-
-for i in range(1,M+1):
-    px += sum(cf[:,i]*np.exp(-r*t[i]))
-px = px/N
-print(px)
-
-# S_t = LSM_put(S,K,T,r,σ,M,N)
-
-#%%
-S = 36
-K = 40
-T = 1
-r = .06
-σ = .2
-m = 50
-M = int(m*T)
-N = 100000
 Δt = T/M
 discount_factor = np.exp(-r * Δt)
-poly_degree = 5
-
-Z = rnd.normal(size=(N,M))
+Z = rnd.normal(size=(int(N/2),M))
+Z = np.concatenate((Z,-Z))
 S_t = np.zeros((N,M+1)) # create matrix for all paths
 S_t[:,0] = S # intitialize S_0 for all paths
 a1 = (r - .5*σ**2)*Δt # constant calculated outside of loop to reduce flops required
@@ -171,10 +82,17 @@ for k in range(M):
 payoff = np.maximum(K - S_t, np.zeros_like(S_t))
 value = np.zeros_like(payoff)
 value[:, -1] = payoff[:, -1]
+X = S_t[:,1].reshape(1,-1)
+Y = payoff[:, -1]
+rf = RandomForestRegressor(n_estimators=10, max_depth=5, min_samples_leaf=100)
+rf_reg = rf.fit(X,Y)
 
 for t in tqdm(range(M-1 , 0 , -1)):
-    regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
-    continuation_value = np.polyval(regression, S_t[:, t])
+    if method == 'LS':
+        regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
+        continuation_value = np.polyval(regression, S_t[:, t])
+    elif method == 'RF':
+        continuation_value = rf.predict(S_t[:, t].reshape(1,-1))
     value[:, t] = np.where(
         payoff[:, t] > continuation_value,
         payoff[:, t],
@@ -183,3 +101,15 @@ for t in tqdm(range(M-1 , 0 , -1)):
 option_premium = np.mean(value[:, 1] * discount_factor)
 print(option_premium)
 
+#%%
+
+# 168089005022003074079122026078102108046069006038058066023088087111117002118099001123122025060038021096117067102122102114006090014008073002058066064077123099084073035075071078082126078094096098127004004118091108100089082006108031025121065089118001024&EXT=pdf&INDEX=TRUE
+# https://towardsdatascience.com/example-of-random-forest-application-in-finance-option-pricing-d6ee06356c6e
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+# https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
+# https://builtin.com/data-science/random-forest-python
+
+# we need 1 forest (15 trees) per time point
+# need to simulate first for training data to train model, then resimulate new paths
+# on going through new paths, use the forest associated with that timepoint to compute continuation value
+# proceed as usual
