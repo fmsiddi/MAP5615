50f54373622d333cd08a7a34fb29f14537c1f7fb
Farez Siddiqui <43183190+fmsiddi@users.noreply.github.com>
2023-04-28 22:04:18 -0400

    almost done
    need hyperparameter analysis and some new option types

--- LS Monte Carlo.py
+++ LS Monte Carlo.py
@@ -21,10 +21,10 @@ r = .1
 σ = .25
 m = 10
 M = int(m*T)
-N = 100000
+N = 500000
 poly_degree = 6
 
-def put(S,K,T,r,σ,M,N,method):
+def LS_put(S,K,T,r,σ,M,N,poly_degree):
     Δt = T/M
     discount_factor = np.exp(-r * Δt)
     Z = rnd.normal(size=(int(N/2),M))
@@ -41,12 +41,8 @@ def put(S,K,T,r,σ,M,N,method):
     value[:, -1] = payoff[:, -1]
     
     for t in tqdm(range(M-1 , 0 , -1)):
-        if method == 'LS':
-            regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
-            continuation_value = np.polyval(regression, S_t[:, t])
-        elif method == 'RF':
-            regression = RandomForestRegressor(n_estimators=10, max_depth=5, min_samples_leaf=100)
-            continuation_value = regression.fit(S_t[:, t], value[:, t + 1]*discount_factor)
+        regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
+        continuation_value = np.polyval(regression, S_t[:, t])
         value[:, t] = np.where(
             payoff[:, t] > continuation_value,
             payoff[:, t],
@@ -55,51 +51,94 @@ def put(S,K,T,r,σ,M,N,method):
     option_premium = np.mean(value[:, 1] * discount_factor)
     return option_premium
 
+px = LS_put(S,K,T,r,σ,M,N,poly_degree)
+print('\n',px)
+
 #%%
 
+def RF_put(S,K,T,r,σ,M,N,antithetic,tree_num,depth,min_samples,samples_per_tree,):
+    Δt = T/M
+    discount_factor = np.exp(-r * Δt)
+    if antithetic == False:
+        Z = rnd.normal(size=(N,M))
+    else:
+        Z = rnd.normal(size=(int(N/2),M))
+        Z = np.concatenate((Z,-Z))
+    S_t = np.zeros((N,M+1)) # create matrix for all paths
+    S_t[:,0] = S # intitialize S_0 for all paths
+    a1 = (r - .5*σ**2)*Δt # constant calculated outside of loop to reduce flops required
+    a2 = σ*np.sqrt(Δt) # constant calculated outside of loop to reduce flops required
+    for k in range(M):
+        S_t[:,k+1] = S_t[:,k] * np.exp(a1 + a2*Z[:,k])
+        
+    payoff = np.maximum(K - S_t, np.zeros_like(S_t))
+    value = np.zeros_like(payoff)
+    value[:, -1] = payoff[:, -1]
+    
+    rf = RandomForestRegressor(n_estimators=tree_num, max_depth=depth, min_samples_leaf=min_samples, bootstrap=True, n_jobs=-1, max_samples=samples_per_tree)
+
+    for t in tqdm(range(M-1 , 0 , -1)):
+        X = S_t[:,t].reshape(-1,1)
+        Y = value[:,t+1]
+        rf.fit(X,Y)
+        continuation_value = rf.predict(X)
+        value[:, t] = np.where(
+            payoff[:, t] > continuation_value,
+            payoff[:, t],
+            value[:, t + 1] * discount_factor
+        )
+    option_premium = np.mean(value[:, 1] * discount_factor)
+    return option_premium
+#%%
+    
+# tree_num = 10
+# depth = 5
+# min_samples = 100
+# samples_per_tree = None
+
+# French paper (bermudan) px = 11.987
+# S = 100
+# K = 110
+# T = 1
+# r = .1
+# σ = .25
+# m = 10
+
+
+# Dubrov example (Approx American) px = 6.7389
+
+tree_num = 10
+depth = 5
+min_samples = 100
+samples_per_tree = 1/tree_num
+
 S = 100
-K = 110
+K = 100
 T = 1
-r = .1
-σ = .25
-m = 10
+r = .03
+σ = .2
+m = 252
+
+# tree_num = 10
+# depth = 5
+# min_samples = 100
+# samples_per_tree = 1/tree_num
+
+# Longstaff Schwartz
+# S = 40
+# K = 40
+# T = 1
+# r = .06
+# σ = .2
+# m = 50
+
+
 M = int(m*T)
 N = 100000
-poly_degree = 6
-method = 'RF'
+antithetic = True
 
-Δt = T/M
-discount_factor = np.exp(-r * Δt)
-Z = rnd.normal(size=(int(N/2),M))
-Z = np.concatenate((Z,-Z))
-S_t = np.zeros((N,M+1)) # create matrix for all paths
-S_t[:,0] = S # intitialize S_0 for all paths
-a1 = (r - .5*σ**2)*Δt # constant calculated outside of loop to reduce flops required
-a2 = σ*np.sqrt(Δt) # constant calculated outside of loop to reduce flops required
-for k in range(M):
-    S_t[:,k+1] = S_t[:,k] * np.exp(a1 + a2*Z[:,k])
-    
-payoff = np.maximum(K - S_t, np.zeros_like(S_t))
-value = np.zeros_like(payoff)
-value[:, -1] = payoff[:, -1]
-X = S_t[:,1].reshape(1,-1)
-Y = payoff[:, -1]
-rf = RandomForestRegressor(n_estimators=10, max_depth=5, min_samples_leaf=100)
-rf_reg = rf.fit(X,Y)
-
-for t in tqdm(range(M-1 , 0 , -1)):
-    if method == 'LS':
-        regression = np.polyfit(S_t[:, t], value[:, t + 1]*discount_factor,poly_degree)
-        continuation_value = np.polyval(regression, S_t[:, t])
-    elif method == 'RF':
-        continuation_value = rf.predict(S_t[:, t].reshape(1,-1))
-    value[:, t] = np.where(
-        payoff[:, t] > continuation_value,
-        payoff[:, t],
-        value[:, t + 1] * discount_factor
-    )
-option_premium = np.mean(value[:, 1] * discount_factor)
-print(option_premium)
+px = RF_put(S,K,T,r,σ,M,N,antithetic,tree_num,depth,min_samples,samples_per_tree)
+print('\n',px)
 
 #%%
 
